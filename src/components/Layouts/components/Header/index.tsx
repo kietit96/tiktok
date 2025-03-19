a1ee4ababd91e8ce8a4ffc3a68304e3664bcbdcb
Anh Kiet <kiet.it.96@gmail.com>
2025-03-19 17:33:12 +0700

    update css

--- src/components/Layouts/components/Header/index.tsx
+++ src/components/Layouts/components/Header/index.tsx
@@ -3,7 +3,7 @@ import images from '@/assets/images'
 import style from './style.module.scss'
 import { NavLink } from 'react-router'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faSpinner, faTimes } from '@fortawesome/free-solid-svg-icons'
 export default function Header() {
   return (
     <div className={style.wrapper}>
@@ -16,16 +16,20 @@ export default function Header() {
             <div className={style.search_form}>
               <div className={style.search_input}>
                 <input className={style.input} placeholder='searching' />
-                <button className={style.search_button_close}>
+                {/* <button className={style.search_button_close}>
                   <FontAwesomeIcon className={`${style.icon} ${style.icon_close}`} icon={faTimes} />
-                </button>
-                <FontAwesomeIcon className={style.search_button_close} icon={faSpinner} spin />
+                </button> */}
+                {/* <div className={style.search_button_loading}>
+                  <FontAwesomeIcon className={`${style.icon} ${style.icon_close}`} icon={faSpinner} spin />
+                </div> */}
               </div>
-              <button className={style.search_button}><img className={`${style.icon} ${style.icon_search}`} src={images.search_icon} /></button>
+              <button className={style.search_button}>
+                <FontAwesomeIcon className={`${style.icon} ${style.icon_search}`} icon={faSearch} />
+              </button>
             </div>
           </div>
           <div className={style.buttons_list}>
-            buttons
+            <FontAwesomeIcon className={style.icon} icon={faSearch} />
           </div>
         </div>
       </div>
